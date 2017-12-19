67cf5a984adfae419e51ce18c894c56e21e8d6eb
Le <liu00414@algonquinlive.com>
2017-12-18 20:44:00 -0500

    make revise according to  professor steve's lecture

--- js/main.js
+++ js/main.js
@@ -1,104 +1,95 @@
-document.addEventListener('DOMContentLoaded', function () {
-    let btnSend = document.querySelector('#btnSend');
-    let btnBack = document.querySelector('#btnBack');
-    btnSend.addEventListener('click', generateNumbers, false);
-    btnSend.addEventListener('click', changeToTab2, false);
-    btnBack.addEventListener('click', changeToTab1, false);
-});
-//make request, generate and display numbers
-function generateNumbers(ev) {
-    ev.preventDefault();
+document.addEventListener('DOMContentLoaded', init);
+
+function init() {
+    document.getElementById('btnSend').addEventListener('click', getNumbers);
+    document.getElementById('btnSend').addEventListener('click', nav);
+    document.getElementById('btnBack').addEventListener('click', nav);
+}
+
+
+
+function getNumbers(ev) {
     let flag = false;
-    console.log('click generate numbers');
-    //    let req = new Request('https://griffis.edumedia.ca/mad9014/lotto/nums.php');
-    let req = new Request('http://localhost/mad9014-lotto/nums.php');
-    let myData = new FormData();
+    let generateMessage = document.querySelector('#message');
+    generateMessage.innerHTML = '';
+    let url = 'http://localhost/mad9014-lotto/nums.php';
+    let fd = new FormData();
     let digits = document.getElementById('digits').value;
     let max = document.getElementById('max').value;
+    //    console.log(parseInt(digits));
+    //    if(parseInt(digits)){};
+    //    if(typeof digits == 'string'&&digits.length>0){}
+    if (parseInt(digits) && parseInt(max)) {
+        fd.append('digits', digits);
+        fd.append('max', max);
+        let info = {
+            method: 'POST',
+            body: fd
+        }
+        fetch(url, info)
+            .then(response =>
+                response.json()
+            )
+            .then(data => {
+                if (data.code == 0) {
+                    //code 0 means there are no errs in the server
+                    //check if there are repeat numbers, if so fetch again
+                    let copy = [];
+                    for (let item of data.numbers) {
+                        if (!copy.includes(item)) {
+                            copy.push(item);
+                        } else {
+                            getNumbers(ev);
+                            flag = true;
+                            break;
+                        }
+                    };
+                    if (flag) {
+                        return;
+                    };
 
-    myData.append('max', max);
-    myData.append('digits', digits);
-    let opts = {
-        method: 'POST',
-        mode: 'CORS',
-        body: myData,
-
-    };
-    fetch(req, opts)
-        .then(function (response) {
-            console.log('response from server', response.status);
-            return response.json();
-
-        })
-        .then(function (data) {
-            console.log(data, data.numbers.length)
-
-            //check
-            let copy = [];
-            for (let item of data.numbers) {
-                if (!copy.includes(item)) {
-                    copy.push(item);
-                } else {
-                    generateNumbers(ev);
-                    flag = true;
-                    break;
+                    let ul = document.querySelector('ul.num_list');
+                    ul.innerHTML = '';
+                    data.numbers.forEach(num => {
+                        let li = document.createElement('li');
+                        li.className = 'num';
+                        li.textContent = num;
+                        ul.appendChild(li);
+                    })
                 }
-            }
-            //            data.numbers.forEach(function(item){
-            //             if (!copy.includes(item)){
-            //                 copy.push(item);
-            //                 
-            //                 }else{generateNumbers(ev);}   
-            //            });
-            //end check
-            if (flag) {
-                return;
-            }
-            let num_list = document.querySelector(".num_list");
-            let message = document.createTextNode(data.message);
-            //        num_list.appendChild(messgae);
-            let generateMessage = document.querySelector('#message');
-            generateMessage.appendChild(message);
+                let message = document.createTextNode(data.message);
+                generateMessage.appendChild(message);
 
-
-            if (data.numbers.length > 0) {
-                for (i = 0; i < data.numbers.length; i++) {
-
-                    let num = document.createTextNode(data.numbers[i]);
-                    console.log(num)
-
-                    let li = document.createElement('li');
-
-                    num_list.appendChild(li);
-                    li.appendChild(num);
-                };
-
-                console.log(num_list);
-            };
-
-        })
-        .catch(function (err) {
-            console.log("ERROR: ", err.message);
+            }).catch(
+        function (err) {
+            //console.log("ERROR: ", err.message);
             let num_list = document.querySelector(".num_list");
 
             let messgae = document.createTextNode(err.message);
             num_list.appendChild(messgae);
 
         });
-};
+    } else {
+        generateMessage.innerHTML = 'Missing required parameters!';
+    }
+}
 
-function changeToTab2(ev) {
-    let tab1 = document.getElementById("home");
-    tab1.classList.add('displaynone');
-    let tab2 = document.getElementById("list");
-    tab2.classList.remove('displaynone');
-};
+function nav(ev) {
+    ev.preventDefault(); //don't let the button navigate away
+    let btn = ev.currentTarget;
+    //console.log(btn);
+    if (btn.id == 'btnBack') {
+        //hide numbers show the form
+        document.getElementById('home').classList.add('active');
+        document.getElementById('list').classList.remove('active');
+        document.querySelector('ul.num_list').innerHTML = '';
+    } else if (btn.id == 'btnSend') {
+        //hide form show the numbers
+        document.getElementById('home').classList.remove('active');
+        document.getElementById('list').classList.add('active');
+        //getNumbers();
+    } else {
+        //none else
+    }
 
-function changeToTab1(ev) {
-    let tab2 = document.getElementById("list");
-    tab2.classList.add('displaynone');
-    let tab1 = document.getElementById("home");
-    tab1.classList.remove('displaynone');
-    document.querySelector('.num_list').innerHTML = '';
-    document.querySelector('#message').innerHTML = '';
 }
